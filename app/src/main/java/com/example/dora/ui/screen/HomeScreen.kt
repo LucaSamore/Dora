f2c65e2c451f78940c1d279b6b31aa757f38fadf
LucaSamore <luca.samore.01@gmail.com>
2023-05-07 09:28:09 +0200

    Test account deletion in home screen

--- app/src/main/java/com/example/dora/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/dora/ui/screen/HomeScreen.kt
@@ -4,25 +4,33 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import arrow.core.Either
 import com.example.dora.viewmodel.HomeViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
     homeViewModel: HomeViewModel,
     onSignOut: () -> Unit
 ) {
+    var error by rememberSaveable { mutableStateOf("Nothing yet") }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Home screen")
-
-        Spacer(modifier = Modifier.padding(vertical = 12.dp))
 
         Spacer(modifier = Modifier.padding(vertical = 12.dp))
 
@@ -34,6 +42,25 @@ fun HomeScreen(
         ) {
             Text(text = "Sign Out")
         }
+
+        Spacer(modifier = Modifier.padding(vertical = 12.dp))
+
+        Button(
+            onClick = {
+                GlobalScope.launch(Dispatchers.Main) {
+                    when (val result = homeViewModel.deleteAccount()) {
+                        is Either.Left -> error = result.value.message
+                        is Either.Right -> onSignOut()
+                    }
+                }
+            }
+        ) {
+            Text(text = "Delete account")
+        }
+
+        Spacer(modifier = Modifier.padding(vertical = 12.dp))
+
+        Text(text = error)
     }
 }
 
--- app/src/main/java/com/example/dora/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/dora/viewmodel/HomeViewModel.kt
@@ -2,9 +2,13 @@ package com.example.dora.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import arrow.core.Either
+import com.example.dora.common.ErrorMessage
 import com.example.dora.repository.auth.AuthenticationRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,5 +18,8 @@ class HomeViewModel @Inject constructor(
 
     fun signOut() = viewModelScope.launch { authenticationRepository.signOut() }
 
-
+    suspend fun deleteAccount(): Either<ErrorMessage, Void> =
+        withContext(viewModelScope.coroutineContext + Dispatchers.IO) {
+            authenticationRepository.deleteUser()
+        }
 }
