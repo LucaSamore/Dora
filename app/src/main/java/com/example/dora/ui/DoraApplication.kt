c76aa2c7fbbba6966b9881d0355664c00d1e6fff
LucaSamore <luca.samore.01@gmail.com>
2023-05-13 12:57:54 +0200

    Add bottom app bar

--- app/src/main/java/com/example/dora/ui/DoraApplication.kt
+++ app/src/main/java/com/example/dora/ui/DoraApplication.kt
@@ -1,14 +1,17 @@
 package com.example.dora.ui
 
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.remember
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.dora.common.Location
+import com.example.dora.ui.navigation.DoraScreen
 import com.example.dora.ui.navigation.NavigationGraph
+import androidx.compose.runtime.getValue
 
 @Composable
 internal fun DoraApplication(
@@ -16,17 +19,29 @@ internal fun DoraApplication(
     location: MutableState<Location>,
     startLocationUpdates: () -> Unit
 ) {
+    val navController = rememberNavController()
     val snackbarHostState = remember { SnackbarHostState() }
+
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = backStackEntry?.destination?.route ?: DoraScreen.SignIn.name
 
     Scaffold(
         snackbarHost = { SnackbarHost(snackbarHostState) },
-        content = {
-            NavigationGraph(
-                navController = rememberNavController(),
-                startDestination = startDestination,
-                location = location,
-                startLocationUpdates = startLocationUpdates,
-            )
+        bottomBar = {
+            if (currentScreen != DoraScreen.SignIn.name && currentScreen != DoraScreen.SignUp.name) {
+                BottomAppBar {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Localized description")
+                    }
+                }
+            }
         }
-    )
+    ) {
+        NavigationGraph(
+            navController = navController,
+            startDestination = startDestination,
+            location = location,
+            startLocationUpdates = startLocationUpdates,
+        )
+    }
 }
