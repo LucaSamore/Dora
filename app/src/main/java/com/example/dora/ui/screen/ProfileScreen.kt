e3f041f4663224aa3b13529eb19ca5c97a7a0f91
LucaSamore <luca.samore.01@gmail.com>
2023-05-17 10:00:48 +0200

    Display user data in profile screen

--- app/src/main/java/com/example/dora/ui/screen/ProfileScreen.kt
+++ app/src/main/java/com/example/dora/ui/screen/ProfileScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import arrow.core.left
 import com.example.dora.common.ErrorMessage
+import com.example.dora.model.User
 import com.example.dora.ui.composable.ProfilePicture
 import com.example.dora.viewmodel.ProfileViewModel
 
@@ -34,103 +35,120 @@ fun ProfileScreen(profileViewModel: ProfileViewModel, modifier: Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        var firstName by rememberSaveable { mutableStateOf("") }
-        var lastName by rememberSaveable { mutableStateOf("") }
-        var emailAddress by rememberSaveable { mutableStateOf("") }
-        var password by rememberSaveable { mutableStateOf("") }
-        var passwordHidden by rememberSaveable { mutableStateOf(true) }
-        var errorMessage by rememberSaveable { mutableStateOf("this is an error") }
-        var errorMessageHidden by rememberSaveable { mutableStateOf(true) }
-        val scope = rememberCoroutineScope()
-        val context = LocalContext.current
-        var imageUri by remember { mutableStateOf<Uri>(Uri.EMPTY) }
-        val galleryLauncher =
-            rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) {
-                imageUri = it ?: Uri.EMPTY
-            }
+        var errorMessage by rememberSaveable { mutableStateOf("") }
+        val eitherUser by profileViewModel.user.collectAsState(initial = ErrorMessage("").left())
 
-        val user by profileViewModel.user.collectAsState(initial = ErrorMessage("").left())
-
-        firstName = user.fold({ e -> e.message }, { u -> u?.location?.latitude.toString() })
-
-        ProfilePicture(context = context)
-
-        Spacer(modifier = modifier.padding(6.dp))
-
-        Row(
-            modifier = modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-        ) {
-            TextButton(onClick = { galleryLauncher.launch("image/*") }) {
-                Text(text = "Get from gallery")
-            }
+        if (eitherUser.isLeft()) {
+            errorMessage = eitherUser.leftOrNull()?.message!!
+            // TODO: Display error message
+        } else {
+            val user = eitherUser.getOrNull()
+            ProfileForm(modifier = modifier, user = user!!)
         }
-
-        if (!errorMessageHidden) {
-            Text(
-                text = errorMessage,
-                color = Color.Red,
-                modifier = modifier.padding(top = 4.dp, bottom = 6.dp)
-            )
-        }
-
-        OutlinedTextField(
-            value = firstName,
-            onValueChange = { firstName = it },
-            label = { Text("First name") },
-            placeholder = { Text("Mario") },
-        )
-
-        Spacer(modifier = modifier.padding(6.dp))
-
-        OutlinedTextField(
-            value = lastName,
-            onValueChange = { lastName = it },
-            label = { Text("Last name") },
-            placeholder = { Text("Rossi") },
-        )
-
-        Spacer(modifier = modifier.padding(6.dp))
-
-        OutlinedTextField(
-            value = emailAddress,
-            onValueChange = { emailAddress = it },
-            label = { Text("Email address") },
-            placeholder = { Text("example@gmail.com") },
-        )
-
-        Spacer(modifier = modifier.padding(6.dp))
-
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            singleLine = true,
-            label = { Text("Password") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            visualTransformation =
-                if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
-            trailingIcon = {
-                IconButton(onClick = { passwordHidden = !passwordHidden }) {
-                    val visibilityIcon =
-                        if (!passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
-                    // Please provide localized description for accessibility services
-                    val description = if (passwordHidden) "Show password" else "Hide password"
-                    Icon(imageVector = visibilityIcon, contentDescription = description)
-                }
-            }
-        )
-
-        Spacer(modifier = modifier.padding(16.dp))
-
-        Button(
-            modifier = modifier.size(TextFieldDefaults.MinWidth, 48.dp),
-            onClick = { errorMessageHidden = !errorMessageHidden }
-        ) {
-            Text("Update profile")
-        }
-
-        Spacer(modifier = modifier.padding(48.dp))
     }
+}
+
+@Composable
+fun ProfileForm(modifier: Modifier, user: User) {
+    var firstName by rememberSaveable { mutableStateOf(user.firstName) }
+    var lastName by rememberSaveable { mutableStateOf(user.lastName) }
+    var emailAddress by rememberSaveable { mutableStateOf(user.emailAddress) }
+    var password by rememberSaveable { mutableStateOf("") }
+    var passwordHidden by rememberSaveable { mutableStateOf(true) }
+    var errorMessage by rememberSaveable { mutableStateOf("") }
+    var errorMessageHidden by rememberSaveable { mutableStateOf(true) }
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+    var imageUri by remember {
+        mutableStateOf<Uri>(
+            if (user.profilePicture != null) Uri.parse(user.profilePicture) else Uri.EMPTY
+        )
+    }
+    val galleryLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) {
+            imageUri = it ?: Uri.EMPTY
+        }
+
+    if (imageUri != Uri.EMPTY) {
+        ProfilePicture(image = imageUri, context = context, defaultAvatar = false)
+    } else {
+        ProfilePicture(context = context)
+    }
+
+    Spacer(modifier = modifier.padding(6.dp))
+
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        TextButton(onClick = { galleryLauncher.launch("image/*") }) {
+            Text(text = "Get from gallery")
+        }
+    }
+
+    if (!errorMessageHidden) {
+        Text(
+            text = errorMessage,
+            color = Color.Red,
+            modifier = modifier.padding(top = 4.dp, bottom = 6.dp)
+        )
+    }
+
+    OutlinedTextField(
+        value = firstName!!,
+        onValueChange = { firstName = it },
+        label = { Text("First name") },
+        placeholder = { Text("Mario") },
+    )
+
+    Spacer(modifier = modifier.padding(6.dp))
+
+    OutlinedTextField(
+        value = lastName!!,
+        onValueChange = { lastName = it },
+        label = { Text("Last name") },
+        placeholder = { Text("Rossi") },
+    )
+
+    Spacer(modifier = modifier.padding(6.dp))
+
+    OutlinedTextField(
+        value = emailAddress!!,
+        onValueChange = { emailAddress = it },
+        label = { Text("Email address") },
+        placeholder = { Text("example@gmail.com") },
+    )
+
+    Spacer(modifier = modifier.padding(6.dp))
+
+    OutlinedTextField(
+        value = password,
+        onValueChange = { password = it },
+        singleLine = true,
+        label = { Text("Password") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        visualTransformation =
+            if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+        trailingIcon = {
+            IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                val visibilityIcon =
+                    if (!passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                val description = if (passwordHidden) "Show password" else "Hide password"
+                Icon(imageVector = visibilityIcon, contentDescription = description)
+            }
+        }
+    )
+
+    Spacer(modifier = modifier.padding(16.dp))
+
+    Button(
+        modifier = modifier.size(TextFieldDefaults.MinWidth, 48.dp),
+        onClick = { errorMessageHidden = !errorMessageHidden }
+    ) {
+        Text("Update profile")
+    }
+
+    Spacer(modifier = modifier.padding(48.dp))
 }
 
 @Preview(showBackground = true)
