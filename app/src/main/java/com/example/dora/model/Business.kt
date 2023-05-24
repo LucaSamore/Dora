387c67ffba685306f40aa51291a62bde24878e9d
LucaSamore <luca.samore.01@gmail.com>
2023-05-25 01:55:56 +0200

    Some improvements to add business screen

--- app/src/main/java/com/example/dora/model/Business.kt
+++ app/src/main/java/com/example/dora/model/Business.kt
@@ -14,8 +14,6 @@ data class Business(
     val website: String? = null,
     val phoneNumber: String? = null,
     val category: Category? = null,
-    val amenities: List<String>? = null,
-    val timeTable: Map<Days, List<TimeSpan>>? = null,
     @field:JvmField val isOpen: Boolean? = null,
     val images: List<String>? = null,
     val createdAt: String = nowWithPattern("yyyy-MM-dd HH:mm:ss")
@@ -24,15 +22,3 @@ data class Business(
         const val collection = "businesses"
     }
 }
-
-enum class Days(val day: String) {
-    SUNDAY("sunday"),
-    MONDAY("monday"),
-    TUESDAY("tuesday"),
-    WEDNESDAY("wednesday"),
-    THURSDAY("thursday"),
-    FRIDAY("friday"),
-    SATURDAY("saturday"),
-}
-
-data class TimeSpan(val start: String, val end: String)
--- app/src/main/java/com/example/dora/ui/screen/AddBusinessScreen.kt
+++ app/src/main/java/com/example/dora/ui/screen/AddBusinessScreen.kt
@@ -2,7 +2,6 @@ package com.example.dora.ui.screen
 
 import android.app.Activity
 import android.net.Uri
-import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -12,6 +11,9 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
@@ -20,6 +22,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.dora.BuildConfig
@@ -43,17 +47,20 @@ internal fun AddBusinessScreen(
     var description by rememberSaveable { mutableStateOf("") }
     var website by rememberSaveable { mutableStateOf("") }
     var phoneNumber by rememberSaveable { mutableStateOf("") }
+    var businessPlace by rememberSaveable { mutableStateOf<BusinessPlace?>(null) }
     val categories = Category.values()
     var expanded by remember { mutableStateOf(false) }
     var selectedOptionText by remember { mutableStateOf(categories[0]) }
-    var businessPlace by rememberSaveable { mutableStateOf<BusinessPlace?>(null) }
     val images = remember { mutableStateListOf<Uri>(Uri.EMPTY) }
+    var showBusinessPlaceName by rememberSaveable { mutableStateOf(false) }
+    var showCarousel by rememberSaveable { mutableStateOf(false) }
     val galleryLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.GetMultipleContents()) {
             images.apply {
                 clear()
                 addAll(it)
             }
+            showCarousel = true
         }
     val intentLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
@@ -69,11 +76,8 @@ internal fun AddBusinessScreen(
                             place.address!!,
                             Location(place.latLng?.latitude!!, place.latLng?.longitude!!)
                         )
-                    Log.i("place", businessPlace.toString())
+                    showBusinessPlaceName = true
                 }
-            } else if (result.resultCode == Activity.RESULT_CANCELED) {
-                // The user canceled the operation.
-                Log.i("place", "User canceled autocomplete")
             }
         }
     val launchMapInputOverlay = {
@@ -87,25 +91,26 @@ internal fun AddBusinessScreen(
 
     Column(
         modifier =
-            modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(paddingValues),
-        verticalArrangement = Arrangement.SpaceAround,
+        modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(paddingValues),
+        verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        HorizontalPager(pageCount = images.size, state = pagerState) { pageNumber ->
-            images.forEachIndexed { index, uri ->
-                if (pageNumber == index) {
-                    AsyncImage(
-                        model = ImageRequest.Builder(context).data(uri).crossfade(true).build(),
-                        contentDescription = "Business photo ${pageNumber + 1}",
-                    )
-                    return@forEachIndexed
+        if (showCarousel) {
+            HorizontalPager(pageCount = images.size, state = pagerState) { pageNumber ->
+                images.forEachIndexed { index, uri ->
+                    if (pageNumber == index) {
+                        AsyncImage(
+                            model = ImageRequest.Builder(context).data(uri).size(320,320).crossfade(true).build(),
+                            contentDescription = "Business photo ${pageNumber + 1}",
+                        )
+                        return@forEachIndexed
+                    }
                 }
             }
         }
-
-        Button(onClick = { galleryLauncher.launch("image/*") }) { Text(text = "Get images") }
-
-        Button(onClick = launchMapInputOverlay) { Text(text = "Test maps") }
 
         OutlinedTextField(
             value = name,
@@ -131,43 +136,6 @@ internal fun AddBusinessScreen(
                 )
         )
 
-        ExposedDropdownMenuBox(
-            expanded = expanded,
-            onExpandedChange = { expanded = !expanded },
-        ) {
-            TextField(
-                // The `menuAnchor` modifier must be passed to the text field for correctness.
-                modifier = modifier.menuAnchor(),
-                readOnly = true,
-                value = selectedOptionText.categoryName,
-                onValueChange = {},
-                label = { Text("Category") },
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                colors =
-                    ExposedDropdownMenuDefaults.textFieldColors(
-                        focusedContainerColor = MaterialTheme.colorScheme.background,
-                        unfocusedContainerColor = MaterialTheme.colorScheme.background,
-                        focusedTextColor = MaterialTheme.colorScheme.onPrimary,
-                        unfocusedTextColor = MaterialTheme.colorScheme.onPrimary,
-                    ),
-            )
-            ExposedDropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-            ) {
-                categories.forEach { selectionOption ->
-                    DropdownMenuItem(
-                        text = { Text(selectionOption.categoryName) },
-                        onClick = {
-                            selectedOptionText = selectionOption
-                            expanded = false
-                        },
-                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
-                    )
-                }
-            }
-        }
-
         OutlinedTextField(
             value = website,
             onValueChange = { website = it },
@@ -191,10 +159,82 @@ internal fun AddBusinessScreen(
                     focusedLabelColor = MaterialTheme.colorScheme.onPrimary,
                 )
         )
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded },
+        ) {
+            TextField(
+                modifier = modifier.menuAnchor(),
+                readOnly = true,
+                value = selectedOptionText.categoryName,
+                onValueChange = {},
+                label = { Text("Category") },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                colors =
+                ExposedDropdownMenuDefaults.textFieldColors(
+                    focusedContainerColor = MaterialTheme.colorScheme.background,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.background,
+                    focusedTextColor = MaterialTheme.colorScheme.onPrimary,
+                    unfocusedTextColor = MaterialTheme.colorScheme.onPrimary,
+                ),
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+            ) {
+                categories.forEach { selectionOption ->
+                    DropdownMenuItem(
+                        text = { Text(selectionOption.categoryName) },
+                        onClick = {
+                            selectedOptionText = selectionOption
+                            expanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                    )
+                }
+            }
+        }
+
+        if (showBusinessPlaceName) {
+            Text(text = businessPlace?.name!!)
+        }
+
+        Button(
+            modifier = modifier.size(TextFieldDefaults.MinWidth, 48.dp),
+            onClick = launchMapInputOverlay
+        ) {
+            Icon(Icons.Filled.Search, "Search place")
+            Spacer(modifier = modifier.size(6.dp))
+            Text(text = "Add business address")
+        }
+
+        Button(
+            modifier = modifier.size(TextFieldDefaults.MinWidth, 48.dp),
+            onClick = {
+                galleryLauncher.launch("image/*")
+            }
+        ) {
+            Icon(Icons.Filled.Photo, "Browse gallery")
+            Spacer(modifier = modifier.size(6.dp))
+            Text(text = "Add business images")
+        }
+
+        Button(
+            modifier = modifier.size(TextFieldDefaults.MinWidth, 48.dp),
+            onClick = { }
+        ) {
+            Text(
+                text = "Create",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        }
     }
 }
 
 /**
  * name [X] description [X] address (street name, number, city, country) [X] website [X] phoneNumber
- * [X] category [X] amenities timeTable isOpen images
+ * [X] category [X] isOpen [] images [X]
  */
