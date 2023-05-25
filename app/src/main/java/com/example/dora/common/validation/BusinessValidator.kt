985e83cba925d7d4bd6994a8f4699292e88b4353
LucaSamore <luca.samore.01@gmail.com>
2023-05-25 20:17:08 +0200

    Create business validator

---
+++ app/src/main/java/com/example/dora/common/validation/BusinessValidator.kt
@@ -0,0 +1,49 @@
+package com.example.dora.common.validation
+
+import android.net.Uri
+import com.example.dora.common.BusinessPlace
+import org.apache.commons.validator.routines.UrlValidator
+
+object BusinessValidator {
+    private const val NAME_MIN_LENGTH = 2
+    private const val NAME_MAX_LENGTH = 50
+    private const val PHONE_NUMBER_LENGTH = 10
+    private const val MAX_IMAGES_NUMBER = 5
+
+    internal fun validateName(name: String): ValidationResult =
+        Validator.validate(
+            name,
+            { n -> Pair(n.isEmpty(), "Business name is required") },
+            { n -> Pair(n.length <= NAME_MIN_LENGTH, "Business name is too short") },
+            { n -> Pair(n.length >= NAME_MAX_LENGTH, "Business name is too long") }
+        )
+
+    internal fun validateAddress(address: BusinessPlace?): ValidationResult =
+        Validator.validate(
+            address,
+            { a -> Pair(a != null, "Address is required") },
+        )
+
+    internal fun validatePhoneNumber(phoneNumber: String): ValidationResult =
+        Validator.validate(
+            phoneNumber,
+            { n ->
+                Pair(
+                    n.length == PHONE_NUMBER_LENGTH,
+                    "Phone number must be $PHONE_NUMBER_LENGTH numbers"
+                )
+            },
+        )
+
+    internal fun validateWebsite(website: String): ValidationResult =
+        Validator.validate(
+            website,
+            { w -> Pair(UrlValidator().isValid(w), "Website url is not valid") },
+        )
+
+    internal fun validateImages(images: List<Uri>): ValidationResult =
+        Validator.validate(
+            images,
+            { i -> Pair(i.size <= MAX_IMAGES_NUMBER, "At most $MAX_IMAGES_NUMBER are allowed") },
+        )
+}
