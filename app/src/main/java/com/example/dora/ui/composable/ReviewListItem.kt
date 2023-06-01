3da4957be7c6f7b787fec884f5b8e01c560d7de8
LucaSamore <luca.samore.01@gmail.com>
2023-06-01 15:02:06 +0200

    Improve review list item

--- app/src/main/java/com/example/dora/ui/composable/ReviewListItem.kt
+++ app/src/main/java/com/example/dora/ui/composable/ReviewListItem.kt
@@ -2,14 +2,19 @@ package com.example.dora.ui.composable
 
 import android.net.Uri
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDownward
 import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -20,46 +25,59 @@ import com.example.dora.model.Review
 @Composable
 internal fun ReviewListItem(review: Review, modifier: Modifier) {
   val context = LocalContext.current
+  var votes by remember { mutableStateOf(review.votes!!) }
+  var downvoteEnabled by remember { mutableStateOf(false) }
+  var upvoteEnabled by remember { mutableStateOf(false) }
 
   Column(
     modifier = modifier.fillMaxWidth().padding(horizontal = 12.dp, vertical = 6.dp),
   ) {
-    Row(
+    LazyRow(
       modifier = modifier.fillMaxWidth().padding(vertical = 12.dp),
       horizontalArrangement = Arrangement.SpaceBetween,
       verticalAlignment = Alignment.CenterVertically
     ) {
-      Row(
-        verticalAlignment = Alignment.Top,
-      ) {
-        ProfilePicture(
-          image = Uri.parse(review.user?.profilePicture),
-          context = context,
-          size = Pair(64.dp, 64.dp),
-          defaultAvatar = false,
-        )
-
-        Column(
-          modifier = modifier.padding(horizontal = 12.dp),
-          verticalArrangement = Arrangement.Top,
-          horizontalAlignment = Alignment.Start
+      item {
+        Row(
+          verticalAlignment = Alignment.Top,
         ) {
-          Text(
-            text = "${review.user?.firstName} ${review.user?.lastName}",
-            style = MaterialTheme.typography.bodyLarge,
-            fontWeight = FontWeight.Bold,
-            modifier = modifier.padding(vertical = 4.dp),
+          ProfilePicture(
+            image = Uri.parse(review.user?.profilePicture),
+            context = context,
+            size = Pair(64.dp, 64.dp),
+            defaultAvatar = false,
           )
-          Row {
-            repeat(review.rating!!) { Icon(Icons.Filled.Star, contentDescription = "Rating star") }
+
+          Column(
+            modifier = modifier.padding(horizontal = 12.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+          ) {
+            Text(
+              text = "${review.user?.firstName} ${review.user?.lastName}",
+              style = MaterialTheme.typography.bodyLarge,
+              fontWeight = FontWeight.Bold,
+              modifier = modifier.padding(vertical = 4.dp),
+            )
+            Row {
+              repeat(review.rating!!) {
+                Icon(Icons.Filled.Star, contentDescription = "Rating star")
+              }
+            }
           }
         }
       }
+      item {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+          IconButton(onClick = { votes -= 1 }, enabled = downvoteEnabled) {
+            Icon(Icons.Filled.ArrowDownward, contentDescription = "Down vote")
+          }
+          Text(text = votes.toString(), modifier = modifier.padding(horizontal = 6.dp))
 
-      Row {
-        Icon(Icons.Filled.ArrowDownward, contentDescription = "Down vote")
-        Text(text = review.votes.toString(), modifier = modifier.padding(horizontal = 6.dp))
-        Icon(Icons.Filled.ArrowUpward, contentDescription = "Up vote")
+          IconButton(onClick = { votes += 1 }, enabled = upvoteEnabled) {
+            Icon(Icons.Filled.ArrowUpward, contentDescription = "Up vote")
+          }
+        }
       }
     }
     Text(text = review.content!!)
