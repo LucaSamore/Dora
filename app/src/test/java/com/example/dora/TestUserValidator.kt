8c779ec9be272da035231b2aa7af5a002004a051
LucaSamore <luca.samore.01@gmail.com>
2023-06-04 18:56:43 +0200

    Update test user validator

--- app/src/test/java/com/example/dora/TestUserValidator.kt
+++ app/src/test/java/com/example/dora/TestUserValidator.kt
@@ -1,7 +1,7 @@
 package com.example.dora
 
-import com.example.dora.common.validation.Status
 import com.example.dora.common.validation.UserValidator
+import com.example.dora.common.validation.Validator
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -9,29 +9,32 @@ class TestUserValidator {
 
   @Test
   fun `Test login credentials validation PASS`() {
-    assertEquals(UserValidator.validateEmailAddress("test@gmail.com").status, Status.PASS)
+    assertEquals(UserValidator.validateEmailAddress("test@gmail.com").status, Validator.Status.PASS)
 
-    assertEquals(UserValidator.validatePassword("Test123!").status, Status.PASS)
+    assertEquals(UserValidator.validatePassword("Test123!").status, Validator.Status.PASS)
   }
 
   @Test
   fun `Test login credentials validation REJECT`() {
-    assertEquals(UserValidator.validateEmailAddress("invalid_email").status, Status.REJECT)
+    assertEquals(
+      UserValidator.validateEmailAddress("invalid_email").status,
+      Validator.Status.REJECT
+    )
 
-    assertEquals(UserValidator.validatePassword("invalid_password").status, Status.REJECT)
+    assertEquals(UserValidator.validatePassword("invalid_password").status, Validator.Status.REJECT)
   }
 
   @Test
   fun `Test register credentials validation PASS`() {
-    assertEquals(UserValidator.validateFirstOrLastName("Mario").status, Status.PASS)
+    assertEquals(UserValidator.validateFirstOrLastName("Mario").status, Validator.Status.PASS)
 
-    assertEquals(UserValidator.validateFirstOrLastName("Rossi").status, Status.PASS)
+    assertEquals(UserValidator.validateFirstOrLastName("Rossi").status, Validator.Status.PASS)
   }
 
   @Test
   fun `Test register credentials validation REJECT`() {
-    assertEquals(UserValidator.validateFirstOrLastName("M").status, Status.REJECT)
+    assertEquals(UserValidator.validateFirstOrLastName("M").status, Validator.Status.REJECT)
 
-    assertEquals(UserValidator.validateFirstOrLastName("R").status, Status.REJECT)
+    assertEquals(UserValidator.validateFirstOrLastName("R").status, Validator.Status.REJECT)
   }
 }
